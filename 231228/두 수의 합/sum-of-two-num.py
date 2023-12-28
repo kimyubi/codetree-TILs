55e55d21dae4dd1d1e3df90f46b3247d37fe39c3
김유비 <62657545+kimyubi@users.noreply.github.com>
2023-12-28 09:38:58 +0900

    Title: 두 수의 합, Time: 177ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231228/두 수의 합/sum-of-two-num.py
+++ 231228/두 수의 합/sum-of-two-num.py
@@ -3,11 +3,18 @@ from collections import defaultdict
 # 원소의 개수 n, 두 수의 합이 될 k
 n, k = map(int, input().split())
 data = list(map(int, input().split()))
-dic = defaultdict(int)
+data.sort()
 
-for i in range(n):
-    for j in range(i+1, n):
-        dic[data[i] + data[j]] += 1
+s, e = 0, n-1
+ans = 0
+while s < e:
+    sum_two = data[s] + data[e]
+    if sum_two == k:
+        ans += 1
+        s += 1
+    elif sum_two < k:
+        s += 1
+    else:
+        e -= 1
 
-
-print(dic[k])
+print(ans)
