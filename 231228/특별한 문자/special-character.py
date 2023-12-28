a14f0befaff974e574f38bf2aa22bbef67fd8cb5
김유비 <62657545+kimyubi@users.noreply.github.com>
2023-12-28 10:45:21 +0900

    Title: 특별한 문자, Time: 271ms, Memory: 73MB, Status: Passed - CodeTree

--- 231228/특별한 문자/special-character.py
+++ 231228/특별한 문자/special-character.py
@@ -2,12 +2,10 @@ from collections import Counter
 data = input().rstrip()
 counter = Counter(data)
 
-exist = False
-for x in data:
-    if counter[x] == 1:
-        print(x)
-        exist = True
-        break
+def solution():
+    for x in data:
+        if counter[x] == 1:
+            return x
+    return None
 
-if not exist:
-    print(None)
+print(solution())
