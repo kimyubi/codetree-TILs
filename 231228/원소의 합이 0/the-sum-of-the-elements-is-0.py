f9a6d7f6d229cb101bb57d9368a35220462b57ad
김유비 <62657545+kimyubi@users.noreply.github.com>
2023-12-28 10:30:24 +0900

    Title: 원소의 합이 0, Time: 325ms, Memory: 54MB, Status: Passed - CodeTree

--- 231228/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 231228/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -1,21 +1,16 @@
 from collections import defaultdict
-import sys
 
-input = sys.stdin.readline
 n = int(input())
 data = [list(map(int, input().split())) for _ in range(4)]
+ans = 0
 
-dic_1 = defaultdict(int)
+dic = defaultdict(int)
 for a in data[0]:
     for b in data[1]:
-        dic_1[a + b] += 1
+        dic[a + b] += 1
 
-dic_2 = defaultdict(int)
 for c in data[2]:
     for d in data[3]:
-        dic_2[c + d] += 1
+        ans += dic[-(c + d)]
 
-ans = 0
-for key, value in dic_1.items():
-    ans += value * dic_2[-key]
 print(ans)
