b8f9397f97867cd9e5ebde50409bb26d0c55b2d7
김유비 <62657545+kimyubi@users.noreply.github.com>
2023-12-28 11:28:36 +0900

    Title: 수열에서의 증가와 감소, Time: 136ms, Memory: 31MB, Status: Passed - CodeTree

--- 231228/수열에서의 증가와 감소/increase-and-decrease-in-sequence.py
+++ 231228/수열에서의 증가와 감소/increase-and-decrease-in-sequence.py
@@ -1,25 +1,24 @@
 n = int(input())
 data = list(map(int, input().split()))
 
-ans1 = 0
-ans2 = 0
+ans = 0
 
-tmp1 = 1
+tmp = 1
 for i in range(1, n):
     if data[i-1] <= data[i]:
-        tmp1 += 1
+        tmp += 1
     else:
-        ans1 = max(ans1, tmp1) 
-        tmp1 = 1
-ans1 = max(ans1, tmp1) 
+        ans = max(ans, tmp) 
+        tmp = 1
+ans = max(ans, tmp) 
 
-tmp2 = 1
+tmp = 1
 for i in range(1, n):
     if data[i-1] >= data[i]:
-        tmp2 += 1
+        tmp += 1
     else:
-        ans2 = max(ans2, tmp2) 
-        tmp2 = 1
-ans2 = max(ans2, tmp2) 
+        ans = max(ans, tmp) 
+        tmp = 1
+ans = max(ans, tmp) 
 
-print(max(ans1, ans2))
+print(ans)
