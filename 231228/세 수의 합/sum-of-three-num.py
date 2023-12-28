7de3e981b70b962fd58ab39b8b8908ec98f6a39f
김유비 <62657545+kimyubi@users.noreply.github.com>
2023-12-28 10:04:20 +0900

    Title: 세 수의 합, Time: 326ms, Memory: 29MB, Status: Passed - CodeTree

--- 231228/세 수의 합/sum-of-three-num.py
+++ 231228/세 수의 합/sum-of-three-num.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 n, k = map(int, input().split())
 data = list(map(int, input().split()))
 ans = 0
+
 for i in range(2, n):
     target = k - data[i]
     dic = defaultdict(int)
