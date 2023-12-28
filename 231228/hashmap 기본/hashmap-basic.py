28b6a06e6d0f8e46ffe200114e4c33f573047a4a
김유비 <62657545+kimyubi@users.noreply.github.com>
2023-12-28 09:10:23 +0900

    Title: hashmap 기본, Time: 185ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 231228/hashmap 기본/hashmap-basic.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+dic = dict()
+for _ in range(n):
+    data = input().split()
+    command = data[0]
+
+    if command == 'add':
+        k, v = int(data[1]), int(data[2])
+        dic[k] = v
+
+    elif command == 'remove':
+        k = int(data[1])
+        dic.pop(k)
+    
+    else:
+        k = int(data[1])
+        print(dic[k] if k in dic else None)
