f15449bad16a1a35eab594d6bb5d51613a8d3a18
김유비 <62657545+kimyubi@users.noreply.github.com>
2023-12-28 11:37:51 +0900

    Title: 1로 만드는 연산, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231228/1로 만드는 연산/operation-to-1.py
+++ 231228/1로 만드는 연산/operation-to-1.py
@@ -1,20 +1,20 @@
-import sys
-ans = sys.maxsize
+n = int(input())
+cnt = 0
 
-def solution(depth, n):
-    global ans
-    if n <= 1:
-        ans = min(ans, depth)
-        return
-
+while True:
+    if n == 1:
+        print(cnt)
+        break
+    
     if not n % 3:
-        solution(depth + 1, n // 3)
+        n //= 3
+        cnt += 1
+        continue
     
     if not n % 2:
-        solution(depth + 1, n // 2)
+        n //= 2
+        cnt += 1
+        continue
     
-    solution(depth + 1, n-1)
-
-n = int(input())
-solution(0, n)
-print(ans)
+    n -= 1
+    cnt += 1
