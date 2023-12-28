76c0dbb79cf325f85e97b0abcf828b8d79a6aaaf
김유비 <62657545+kimyubi@users.noreply.github.com>
2023-12-28 11:15:34 +0900

    Title: 수열에서의 증가와 감소, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231228/수열에서의 증가와 감소/increase-and-decrease-in-sequence.py
@@ -0,0 +1,17 @@
+n = int(input())
+data = list(map(int, input().split()))
+
+dp_increase = [1] * n
+dp_decrease = [1] * n
+
+for i in range(n):
+    for j in range(i):
+        if data[j] < data[i]:
+            dp_increase[i]= max(dp_increase[j] + 1, dp_increase[i])
+
+for i in range(n):
+    for j in range(i):
+        if data[i] < data[j]:
+            dp_decrease[i]= max(dp_decrease[j] + 1, dp_decrease[i])
+
+print(max(max(dp_decrease), max(dp_increase)))
