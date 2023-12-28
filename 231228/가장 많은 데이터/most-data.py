c00d6f3585bd2583733e6e0d26a3afe33cdba407
김유비 <62657545+kimyubi@users.noreply.github.com>
2023-12-28 09:22:14 +0900

    Title: 가장 많은 데이터, Time: 298ms, Memory: 35MB, Status: Passed - CodeTree

--- 231228/가장 많은 데이터/most-data.py
+++ 231228/가장 많은 데이터/most-data.py
@@ -1,5 +1,14 @@
-from collections import Counter
+# from collections import Counter
+# n = int(input())
+# data = [input() for _ in range(n)]
+# counter = Counter(data)
+# print(counter.most_common(1)[0][1])
+
+
+from collections import defaultdict
 n = int(input())
-data = [input() for _ in range(n)]
-counter = Counter(data)
-print(counter.most_common(1)[0][1])
+dic = defaultdict(int)
+for _ in range(n):
+    dic[input()] += 1
+
+print(max(dic.values()))
