2349d1abe92969f402c83450997a70df0de99ff9
김유비 <62657545+kimyubi@users.noreply.github.com>
2023-12-28 11:46:55 +0900

    Title: 1로 만드는 연산, Time: 158ms, Memory: 31MB, Status: Passed - CodeTree

--- 231228/1로 만드는 연산/operation-to-1.py
+++ 231228/1로 만드는 연산/operation-to-1.py
@@ -1,16 +1,13 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 dp = [0] * (n + 1)
 
 for i in range(2, n + 1):
     dp[i] = dp[i-1] + 1
     
-    if i % 2 == 0:
+    if not i % 2:
         dp[i] = min(dp[i], dp[i//2] + 1)
     
-    if i % 3 == 0:
+    if not i % 3:
         dp[i] = min(dp[i], dp[i//3] + 1)
         
 print(dp[n])
