3e94ac498ccd407f76405cfe725a089b99f1d180
김유비 <62657545+kimyubi@users.noreply.github.com>
2023-12-30 22:35:20 +0900

    Title: 자리 바꾸기 2, Time: 735ms, Memory: 68MB, Status: Passed - CodeTree

--- 231230/자리 바꾸기 2/changing-seats-2.py
+++ 231230/자리 바꾸기 2/changing-seats-2.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 dic = defaultdict(set)
-[0, 1, 4, 2, 3, 5]
 n, k = map(int, input().split())
 position = [i for i in range(n + 1)]
 data = [list(map(int, input().split())) for _ in range(k)]
@@ -12,7 +11,6 @@ for _ in range(3):
     for a, b in data:
         dic[position[a]].add(b)
         dic[position[b]].add(a)
-
         position[a], position[b] = position[b], position[a]
 
 for i in range(1, n + 1):
