9ea93eac635013fb2dcaa327d08609b2780e7dfd
김유비 <62657545+kimyubi@users.noreply.github.com>
2024-01-02 18:35:09 +0900

    Title: 대칭 차집합, Time: 372ms, Memory: 79MB, Status: Passed - CodeTree

--- 240102/대칭 차집합/symmetric-difference-set.py
+++ 240102/대칭 차집합/symmetric-difference-set.py
@@ -1,6 +1,9 @@
+import sys
+input = sys.stdin.readline
 n, m = map(int, input().split())
-set_1 = set(list(map(int,input().split())))
-set_2 = set(list(map(int,input().split())))
+
+set_1 = set(map(int,input().split()))
+set_2 = set(map(int,input().split()))
 
 
-print(len((set_1 - set_2) | (set_2-set_1)))
+print(len((set_1 - set_2)| (set_2-set_1)))
