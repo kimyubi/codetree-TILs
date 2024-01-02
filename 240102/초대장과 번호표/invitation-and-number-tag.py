628b9c5d52e5972438c58536be270147bc9a91af
김유비 <62657545+kimyubi@users.noreply.github.com>
2024-01-02 18:24:13 +0900

    Title: 초대장과 번호표, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240102/초대장과 번호표/invitation-and-number-tag.py
+++ 240102/초대장과 번호표/invitation-and-number-tag.py
@@ -6,7 +6,9 @@ invitation.add(1)
 for idx in range(g):
     data = list(map(int, input().split()))
     cnt, group = data[0], data[1:]
+    invitation.add(group[0])
     
+    # diff는 초대 못 받은 사람
     diff = set(group) - invitation
     if len(diff) == 1:
         invitation.add(diff.pop())
