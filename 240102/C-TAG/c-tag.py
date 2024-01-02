96690e86cc72cc8e7a51e06f8356190f70daf54f
김유비 <62657545+kimyubi@users.noreply.github.com>
2024-01-02 17:49:49 +0900

    Title: C-TAG, Time: 4180ms, Memory: 26MB, Status: Passed - CodeTree

--- 240102/C-TAG/c-tag.py
+++ 240102/C-TAG/c-tag.py
@@ -4,20 +4,18 @@ n, m = map(int, input().split())
 
 group_a = [list(input()) for _ in range(n)]
 group_b = [list(input()) for _ in range(n)]
-arr = [i for i in range(m)]
+comb = combinations([i for i in range(m)], 3)
 
 ans = 0
-for x, y, z in combinations(arr, 3):
-    dedicate_a = []
+for x, y, z in comb:
+    dedicate_a = set()
     for row in group_a:
-        dedicate_a.append((row[x], row[y], row[z]))
+        dedicate_a.add((row[x], row[y], row[z]))
 
-    dedicate_b = []
+    dedicate_b = set()
     for row in group_b:
-        dedicate_b.append((row[x], row[y], row[z]))
+        dedicate_b.add((row[x], row[y], row[z]))
 
-    dedicate_a, dedicate_b = set(dedicate_a), set(dedicate_b)
-    
     if not (dedicate_a & dedicate_b):
         ans += 1
 
